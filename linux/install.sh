718d02aa01b410581f1f2e2b3f714ccea9d35238
nrhr884 <naruhiro@debian00>
2015-05-01 21:57:38 +0900

    fixed install.sh

--- linux/install.sh
+++ linux/install.sh
@@ -5,7 +5,14 @@ ln -sf ~/vim-env/_vimrc ~/.vimrc
 mkdir ~/bundle
 cd ~/bundle
 git clone https://github.com/Shougo/neobundle.vim
-#install ag
-sudo apt-get install silversearcher-ag
 #install global
 sudo apt-get install global
+#install ag
+mkdir ~/envtmp
+cd envtmp
+git clone https://github.com/ggreer/the_silver_searcher || exit 1
+sudo apt-get install -y automake pkg-config libpcre3-dev zlib1g-dev liblzma-dev || exit 1
+cd the_silver_searcher
+./build.sh
+sudo make install
+rm -rf ~/envtmp
